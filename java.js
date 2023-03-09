ac9552da61b3eeda3be89b772b3c35c478a1d2b7
Conrado Simon <conradosimon2013@gmail.com>
2023-03-09 20:05:29 -0300

    Se agrego movimientos blockeados por otro jugador y golpe

--- java.js
+++ java.js
@@ -1,34 +1,69 @@
 
+var pj1 = document.getElementById('pj1');
+var pj2 = document.getElementById('pj2');
+
 let pj1left = 0
 let pj1right= 0
 let positionpj1 = 10
 let pj1golpe = 0
+
+let pj2left = 0
+let pj2right= 0
+let positionpj2 = 1000
+let pj2golpe = 0
+let posepj1 = 1
 	
 function esperar(seg){
-	 setTimeout(function(){ document.getElementById('pj1').style.backgroundImage = 'url(https://www.fightersgeneration.com/characters3/ryu-ts-stance.gif)';pj1golpe = 0}, seg);
+	 setTimeout(function(){pj1golpe = 0}, 380);
+	 setTimeout(function(){ document.getElementById('pj1').style.backgroundImage = 'url(https://www.fightersgeneration.com/characters3/ryu-ts-stance.gif)';}, seg);
 }
 
 setInterval(code, 8);
 
 function code(){
-	if (pj1left==1 && pj1golpe == 0){
-		console.log("asasa")
-		positionpj1+=2
-		document.getElementById('pj1').style.left = `${positionpj1}px`
+	if (pj1left==1 && pj1golpe == 0){		
+
+		if(positionpj1 != (positionpj2-40) ) {positionpj1+=2
+		document.getElementById('pj1').style.left = `${positionpj1}px`}
 		document.getElementById('pj1').style.transform = 'scaleX(1)';
 		document.getElementById('pj1').style.backgroundImage = 'url(https://www.fightersgeneration.com/characters3/ryu-walkf.gif)';
+		posepj1 = 1;
 			
 
 	}
+
 	if (pj1right==1 && pj1golpe == 0 ){
-		console.log("asasa")
-		positionpj1-=2
-		document.getElementById('pj1').style.left = `${positionpj1}px`;
+		
+		if(positionpj1 != (positionpj2+40) ) {positionpj1-=2
+		document.getElementById('pj1').style.left = `${positionpj1}px`;}
 		document.getElementById('pj1').style.transform = 'scaleX(-1)';
 		document.getElementById('pj1').style.backgroundImage = 'url(https://www.fightersgeneration.com/characters3/ryu-walkf.gif)';
+		
+		posepj1 = -1;
 			}
+
 	if (pj1left==0 && pj1right==0 && pj1golpe == 0) {
 		document.getElementById('pj1').style.backgroundImage = 'url(https://www.fightersgeneration.com/characters3/ryu-ts-stance.gif)';
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+	if (pj1left==0 && pj1right==0 && pj1golpe == 0) {
+		document.getElementById('pj2').style.backgroundImage = 'url(https://www.fightersgeneration.com/characters2/ken-ts-stance.gif)';
+
+
+
+
 
 	}
 }
@@ -60,14 +95,32 @@ document.addEventListener("keyup", function(event) {
 document.addEventListener("keydown", function(event) {
     	if(event.key === "j" || event.key === "J"){
     		if(pj1golpe == 0){
-    	document.getElementById('pj1').style.backgroundImage = 'url(https://www.fightersgeneration.com/characters3/ryu-wp.gif)';
+
+	 setTimeout(function(){document.getElementById('pj1').style.backgroundImage = 'url(punch0.png)';}, 50);
+	 setTimeout(function(){document.getElementById('pj1').style.backgroundImage = 'url(punch1.png)';}, 100);
+ 		golpear(pj1)
 		pj1golpe = 1;
 		esperar(500);}
 
     	}
     })
 
+function golpear(pj){ if (pj == pj1) {
 
+
+		if ( positionpj1 <= positionpj2 && (positionpj1+80) > (positionpj2) && posepj1 == 1 ){ 
+	 	setTimeout(function(){document.getElementById('pj2').style.backgroundImage = 'url(https://www.fightersgeneration.com/characters2/ken-hit.gif)';}, 50);
+	 	setTimeout(function(){document.getElementById('pj2').style.backgroundImage = 'url(https://www.fightersgeneration.com/characters2/ken-ts-stance.gif)';}, 300);}
+
+
+		if ( positionpj1 >= positionpj2 && (positionpj1-80) < (positionpj2) && posepj1 == -1 ){ 
+	 	setTimeout(function(){document.getElementById('pj2').style.backgroundImage = 'url(https://www.fightersgeneration.com/characters2/ken-hit.gif)';}, 50);
+	 	setTimeout(function(){document.getElementById('pj2').style.backgroundImage = 'url(https://www.fightersgeneration.com/characters2/ken-ts-stance.gif)';}, 300);}
+
+
+		
+	}
+}
 
 
 
